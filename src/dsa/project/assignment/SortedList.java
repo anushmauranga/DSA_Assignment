9f328bacbdca5324855887b7957e6684b5b88ca5
Anush <Anush@Anush-PC>
2015-03-30 10:49:24 +0530

    Added SortedList class

---
+++ src/dsa/project/assignment/SortedList.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsa.project.assignment;
+
+/**
+ *
+ * @author Anush
+ */
+public class SortedList {
+    
+}
