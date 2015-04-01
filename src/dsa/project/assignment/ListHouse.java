f8dbecc9b1d18b635e8a5dfdf94fedce0327fbff
Damidu <damidudasanayake@gmail.com>
2015-04-01 10:32:26 +0530

    adding code to ListHouse.java

--- src/dsa/project/assignment/ListHouse.java
+++ src/dsa/project/assignment/ListHouse.java
@@ -4,7 +4,34 @@
  */
 package dsa.project.assignment;
 
-
-public class ListHouse {
+public class ListHouse implements Listable 
+{
+    private int lotNumber;
+    private String firstName;
+    private String lastName;
+    private Double price;
+    private int squareFeet;
+    private int  bedRooms;
     
+    //Initial variables with defalut contuctor
+    public ListHouse()
+    {
+     this.lotNumber = 0;
+     this.firstName = "";
+     this.lastName = "";
+     this.price = 0.00;
+     this.squareFeet = 0;
+     this.bedRooms = 0;
+    }
+    
+    //Initial variables with  parameterized constructor
+     public ListHouse(int lotNumber,String firstName,String lastName,Double price,int squareFeet,int  bedRooms)
+    {
+     this.lotNumber = lotNumber;
+     this.firstName = firstName;
+     this.lastName = lastName;
+     this.price = price;
+     this.squareFeet = squareFeet;
+     this.bedRooms = bedRooms;
+    }
 }
